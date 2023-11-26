5f588e1b9c5625edc0ac89aad9dbcc323dbc9064
Ruben Mantilla <-h>
2023-11-25 21:52:29 -0500

    fix: dockerfile add http-server

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM node:18-alpine as builder
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
+RUN npm install -g @angular/cli@16
 COPY . .
 RUN npm run build:prod
 RUN ls -alt
@@ -15,4 +16,9 @@ COPY --from=builder /app/dist ./dist
 COPY --from=builder /app/package.json /app/package-lock.json ./
 
 RUN npm install --omit=dev
-CMD ["npm", "start"]
+# CMD ["npm", "run", "start:prod"]
+
+RUN npm install -g http-server
+
+# Ejecutar el servidor http en el puerto 4200
+CMD ["http-server", "dist/task-list", "-p", "4200"]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { APP_BASE_HREF } from '@angular/common'; // Importa APP_BASE_HREF
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
-  providers: [],
+  providers: [{ provide: APP_BASE_HREF, useValue: '/' }], // Provee el APP_BASE_HREF
   bootstrap: [AppComponent],
 })
 export class AppModule {}
